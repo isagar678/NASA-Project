10934dde83b6ed3c0a432a9c963fafde5807c3ca
Sagar Bharati <bharatisagar678@gmail.com>
2024-12-25 18:04:55 +0530

    build for react

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-
+const path = require('path');
 const {planetsRouter} = require('./routes/planets/planets.router')
 
 
@@ -13,6 +13,7 @@ app.use(cors(
 ))
 app.use(express.json())
 app.use('/',planetsRouter)
+app.use(express.static(path.join(__dirname,'..','public')))
 module.exports={
     app
 }
